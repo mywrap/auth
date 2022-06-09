718efa04892b3aecab7b61feb8eac5bf2a846601
Đào Thanh Tùng <tung.dao@difisoft.com>
2022-06-09 09:44:08 +0700

    personal options in example gen password

--- example/gen_password8.go
+++ example/gen_password8.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mywrap/auth"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		passwd := auth.GenRandomPassword(8)
-		_, _ = fmt.Println, passwd
-		fmt.Println(passwd)
+	for i := 0; i < 128; i++ {
+		p := auth.GenRandomPassword(8)
+
+		if true { // my personal options
+			if strings.Index("abcdefghijklmnopqrstuvwxyz", p[0:1]) == -1 {
+				continue
+			}
+			if p[len(p)-1:] == "_" {
+				continue
+			}
+			isNasty := false
+			for _, nastyChar := range []string{
+				"1", "j", "i", "I", "l", "L",
+				"0", "o", "O",
+				"5", "s", "S",
+			} {
+				if strings.Contains(p, nastyChar) {
+					isNasty = true
+					break
+				}
+			}
+			if isNasty {
+				continue
+			}
+		}
+
+		fmt.Println(p)
 	}
 }
